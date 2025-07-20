5584a053f77a696c4654e2c81bbba7ae0ca5b356
Hemraj Suthar <hemrajct@gmail.com>
2025-07-20 12:40:01 +0530

    remove static.json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import About from './pages/About';
-import ProjectList from './components/ProjectList';
 import ProjectDetail from './components/ProjectDetail';
-import WhatsappLink from './components/WhatsappLink';
 import AppLayout from './pages/AppLayout';
-// import projects from './src/Projects.jsx';
 
 function App() {
     return (
         <Router>
-            {/* <Navbar/> */}
             <Routes>
                 <Route path='/' element={<AppLayout/>} >
                     <Route index element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/project/:id" element={<ProjectDetail />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/about" element={<About />} />
-                    {/* <Route path="*" element={<Home />} /> */}
-                    {/* <Route path="/" element={<ProjectList />} /> */}
+                    <Route path='projects' element={<Projects />} />
+                    <Route path='project/:id' element={<ProjectDetail />} />
+                    <Route path='contact' element={<Contact />} />
+                    <Route path='about' element={<About />} />
                 </Route>
             </Routes>
-            {/* <WhatsappLink/> */}
-            {/* <Footer/> */}
         </Router>
     );
 }
