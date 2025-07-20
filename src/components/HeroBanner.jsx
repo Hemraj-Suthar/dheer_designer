17942cda9de518ffc7b7481ff9cb140dc0d7d1d4
Hemraj Suthar <hemrajct@gmail.com>
2025-07-20 13:32:49 +0530

    add link in herobanner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HeroBanner = () => {
 
@@ -7,9 +8,9 @@ const HeroBanner = () => {
             <section className="max-w-screen-2xl container mx-auto md:px-20 px-4 py-16 text-center">
                 <h1 className="text-5xl font-bold mb-6">DHEER VISUAL STUDIO</h1>
                 <p className="mb-8 max-w-2xl mx-auto">Looking forward to help you bring and provide my skills to solve your next project with a great UI/UX design.</p>
-                <a className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-2xl inline-block text-center" href="/contact">
+                <Link to="/contact" className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-2xl inline-block text-center">
                     Contact me
-                </a>
+                </Link>
             </section>
         </>
     );
